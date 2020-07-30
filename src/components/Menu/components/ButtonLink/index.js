04000fc9284a97c5b722f3ad0b83aeedeb11303c
Pablo-hgit <pablohenrique.ph629@gmail.com>
2020-07-30 19:07:40 -0300

    Setup GameFlix

---
+++ src/components/Menu/components/ButtonLink/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+function ButtonLink(props) {
+    // props => { className; "Do que alguem passar", href:"/"}
+    return (
+    <a href={props.href} className={props.className}>
+       
+       {props.children} 
+    </a>
+    
+    
+    )
+  
+    
+  }
+  
+  export default ButtonLink;
