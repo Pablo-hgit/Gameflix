ef8879648f3616207cd33933b31752bc5933bb60
Pablo-hgit <pablohenrique.ph629@gmail.com>
2020-07-31 22:40:14 -0300

    commit

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <FooterBase>
       <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+        <img src="https://fontmeme.com/permalink/200801/611d75240b09893bb5572cd9be1d6098.png" alt="Logo Alura" />
       </a>
       <p>
         Orgulhosamente criado durante a
