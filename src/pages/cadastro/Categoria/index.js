8dbd4ec0acf87bf3a099941536d35d35db39dbb0
Pablo-hgit <pablohenrique.ph629@gmail.com>
2020-08-02 22:18:07 -0300

    commit

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React, { useState} from 'react';
 import { Link } from 'react-router-dom';
 import PageDefault from '../../../components/PageDefault';
 
 function CadastroCategoria() {
-    return(
+  const [ categorias, setCategorias] = useState (['teste']);
+   const [nomeDaCategoria, setNomeDaCategoria] = useState('Valor Inicial');  
+  return(
       <PageDefault>
-          <h1>Cadastro de categoria</h1>
-      
-      
+          <h1>Cadastro de Categoria: {nomeDaCategoria}</h1>
+          
+          <form onSubmit={function handleSubmit(infosDoEvento){
+            infosDoEvento.preventDefault()
+            console.log('Voce tentou inviar o form ne')
+            setCategorias([
+              ...categorias,
+              nomeDaCategoria
+            ]);
+          }}>
+            {/* State */}
+            <label>
+              Nome da categoria
+              <input
+                 type="text"
+                 value={nomeDaCategoria}
+                 onChange={function Handler(infosDoEvento) {
+                 
+                   setNomeDaCategoria(infosDoEvento.target.Value) 
+                }}
+
+               />
+
+            </label>
+          
+            <button>
+              Cadastrar
+            </button>
+          </form>
+          
+          <ul>
+            {categorias.map((categoria, indice) => {
+              return(
+              <li key={`${categoria}${indice}`}>
+                {categoria}
+              </li>
+              )
+          })}
+          </ul>
       
        <Link to="/">
          Ir para home
