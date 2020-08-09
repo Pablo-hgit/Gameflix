293809ad5becfdd21a87b28261d7c2f6e90122e0
Pablo-hgit <pablohenrique.ph629@gmail.com>
2020-08-08 23:22:27 -0300

    commit de novo

--- src/components/FormField/index.js
+++ src/components/FormField/index.js
@@ -3,54 +3,59 @@ import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
 
 const FormFieldWrapper = styled.div`
- position: relative;
+  position: relative;
   textarea {
     min-height: 150px;
   }
   input[type="color"] {
     padding-left: 56px;
   }
-  
 `;
 
 const Label = styled.label``;
 Label.Text = styled.span`
-
+  color: #e5e5e5;
+  height: 57px;
+  position: absolute;
+  top: 0;
+  left: 16px;
+  display: flex;
+  align-items: center;
+  transform-origin: 0% 0%;
+  font-size: 18px;
+  font-style: normal;
+  font-weight: 300;
+  transition: 0.1s ease-in-out;
 `;
 
 const Input = styled.input`
-  background: #53585D;
-  color: #F5F5F5;
+  background: #53585d;
+  color: #f5f5f5;
   display: block;
   width: 100%;
   height: 57px;
   font-size: 18px;
-  
   outline: 0;
   border: 0;
   border-top: 4px solid transparent;
-  border-bottom: 4px solid #53585D;
-  
+  border-bottom: 4px solid #53585d;
   padding: 16px 16px;
   margin-bottom: 45px;
-  
   resize: none;
   border-radius: 4px;
-  transition: border-color .3s;
-  
+  transition: border-color 0.3s;
   &:focus {
     border-bottom-color: var(--primary);
-    } 
-    
-    &:not([type='color']) + ${Label.Text} {
-          transform: scale(.6) translateY(-10px);
-        }
-    
-    ${({ hasValue }) => hasValue && css`
-     &:not([type='color']) + ${Label.Text} {
-          transform: scale(.6) translateY(-10px);
+  }
+  &:focus:not([type="color"]) + span {
+    transform: scale(0.6) translateY(-10px);
+  }
+  ${({ hasValue }) => hasValue
+    && css`
+      &:not([type="color"]) + span {
+        transform: scale(0.6) translateY(-10px);
       }
-  `}
+    `}
 `;
 
 function FormField({
@@ -60,7 +65,7 @@ function FormField({
   const isTypeTextarea = type === 'textarea';
   const tag = isTypeTextarea ? 'textarea' : 'input';
 
-  const hasValue = Boolean(value.length);
+  const hasValue = Boolean(value);
   const hasSuggetions = Boolean(suggestions.length);
   return (
     <FormFieldWrapper>
@@ -86,7 +91,7 @@ function FormField({
           <datalist id={`suggestionFor_${fieldId}`}>
             {
           suggestions.map((suggestion) => (
-            <option values={suggestion} key={`suggestionFor_${fieldId}_option${suggestion}`}>
+            <option value={suggestion} key={`suggestionFor_${fieldId}_option${suggestion}`}>
               {suggestion}
             </option>
           ))
